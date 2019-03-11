61d33d25781013d414fd44d6c6e737b19de6272e
K <kiyunkim@users.noreply.github.com>
2019-03-11 17:08:05 -0400

    webpack update
    app/ to src/,
    copy over from webpack practice,
    +css minifier
    
    to do: change dist to docs

--- webpack.config.js
+++ webpack.config.js
@@ -1,87 +1,114 @@
 const path = require('path'); // for different operating systems
+const webpack = require('webpack');
+const merge = require('webpack-merge'); // to merge baseConfig
+const CleanWebpackPlugin = require('clean-webpack-plugin'); // clean output
 const HtmlWebpackPlugin = require('html-webpack-plugin'); // webpack only knows js, so add plugin to read html
 const MiniCssExtractPlugin = require('mini-css-extract-plugin'); // to convert into css file
-
-// environment set in cli through scripts
-
-const baseConfig = {
-  devtool: 'cheap-module-eval-source-map',
-  context: path.join(__dirname, 'app'), 
-  entry: {
-    app: './app.js',
-  }, 
-  output: {
-    path: path.join(__dirname, 'app/dist'),
-    filename: '[name].bundle.js'
-  },
-  devServer: {
-    contentBase: path.join(__dirname, 'app'),
-    watchContentBase: true,
-    publicPath: '/dist/',
-    inline: true,
-    stats: 'errors-only',
-    port: 8000,
-  },
-  resolve: {
-    extensions: ['.js','.scss', '.css']
-  },
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        include: /app/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-env'],
-            plugins: ['@babel/plugin-transform-runtime']
-          }
-        }
-      },
-      {
-        test: /\.css$/,
-        include: /app/,
-        exclude: /node_modules/,
-        use: [
-          MiniCssExtractPlugin.loader,
-          'css-loader',
-        ]
-      }
-    ]
-  },
-  plugins: [/* 
-    new HtmlWebpackPlugin({
-      template: path.join(__dirname, 'app', 'index.html'),
-      // filename: path.join(__dirname, 'index.html'),
-      hash: true,
-      chunks: ['app'] // name of the bundle, determined by the key in line 6. add only this bundle
-    }), */
-    new MiniCssExtractPlugin({
-      filename: path.join(__dirname, 'css', '[name].css')
-    })
-  ]
-}
-
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 
 module.exports = function (env) {
-  // use env instead of process.env.NODE_ENV & NODE_ENV=dev for different cli
-  const isDev = env === 'dev';
-  console.log(`${isDev ? 'development' : 'production'} environment`);
+  // environment set in cli through npm scripts
+  const isDev = env.dev;
+  const isProd = env.prod;
 
-  if (isDev) {
-    baseConfig.mode = 'development';
-  } else {
-    baseConfig.mode = 'production';
-    baseConfig.plugins.push(
-      // ..
-    )
+  // config for all env:
+  const baseConfig = {
+    context: path.resolve(__dirname, 'src'), // for './'
+    entry: {
+      app: './js/app.js', // key: file name ('app'), value: path to bundle
+    },
+    output: {
+      path: path.resolve(__dirname, 'dist'),
+      filename: 'js/[name].bundle.js', // [name] grabbed from the entry key
+    },
+    module:  {
+      rules: [
+        {
+          test: /\.scss$/,
+          include: /src/,
+          exclude: /node_modules/,
+          use: [
+            MiniCssExtractPlugin.loader,
+            'css-loader',
+            'sass-loader',
+          ]
+        }
+      ]
+    },
+    plugins: [
+      new webpack.DefinePlugin({ // define constants in code
+        ENV: JSON.stringify(isDev ? 'development' : 'production')
+      }),
+      new CleanWebpackPlugin(),
+      new HtmlWebpackPlugin({
+        title: 'custom title',
+        text: 'some text',
+        template: path.join(__dirname, 'src/index.html'),
+        filename: path.join(__dirname, 'dist/index.html'),
+        inject: 'body', // this is the default, not needed
+        hash: true,
+        chunks: ['app'] // name of the bundle, determined by the key in line 6. add only this bundle
+      }),
+      new MiniCssExtractPlugin({
+        filename: 'css/[name].css'
+      })
+    ]
   }
-  return baseConfig;
-};
 
+  // dev environment
+  if (isDev) {
+    return merge(baseConfig, {
+      mode: 'development',
+      devtool: 'cheap-module-eval-source-map',
+      devServer: {
+        contentBase: path.join(__dirname, 'dist'),
+        watchContentBase: true,
+        inline: true, // dont run my app inside the webpack iframe - this is the default
+        stats: 'errors-only',
+        port: 8000, // number doesnt matter
+      },
+      plugins: [
+        { // see config
+          apply(compiler) {
+            compiler.plugin("done", function() {
+              console.log(require('util').inspect(compiler.options))
+            })
+          }
+        }
+      ]
+    })
+  }
 
-
-
-
-
-
+  if (isProd) {
+    return merge(baseConfig, {
+      mode: 'production',
+      module: {
+        rules: [
+          {
+            test: /\.js$/,
+            include: /src/, // or path.join(__dirname, 'src')
+            use: {
+              loader: 'babel-loader', // compile js only in prod
+              options: {
+                presets: [
+                  ['@babel/preset-env', {
+                    debug: true,
+                    targets: {
+                      browsers: ['defaults']
+                    }
+                  }]
+                ],
+                plugins: ['@babel/plugin-transform-runtime']
+              }
+            }
+          },
+        ]
+      },
+      optimization: {
+        minimizer: [
+          new OptimizeCSSAssetsPlugin({})
+        ]
+      },
+    })
+  }
+}
